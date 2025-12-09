cbfaa62ed7c2a6510a3025a7cf525d541f4c2012
Jaco Labuschagne <zodimo@gmail.com>
2025-12-09 16:47:17 +0200

    composable sequence helper

--- compose/composer.go
+++ compose/composer.go
@@ -6,6 +6,30 @@ import (
 	"go-compose-dev/pkg/api"
 )
 
-func NewComposer(store state.PersistentState) api.Composer {
+type Composable = api.Composable
+type Composer = api.Composer
+
+func NewComposer(store state.PersistentState) Composer {
 	return zipper.NewComposer(store)
+}
+
+type composableSequence struct {
+	contents []Composable
+}
+
+func NewComposableSequence(composables []Composable) *composableSequence {
+	return &composableSequence{contents: composables}
+}
+
+func (s *composableSequence) Compose(c Composer) Composer {
+	for _, content := range s.contents {
+		c = content(c)
+	}
+	return c
+}
+
+func Sequence(contents ...Composable) Composable {
+	return func(c Composer) Composer {
+		return NewComposableSequence(contents).Compose(c)
+	}
 }
