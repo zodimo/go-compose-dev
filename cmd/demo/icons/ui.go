66bb5d4d744c88012419d472fe218b061435a4b6
Jaco Labuschagne <zodimo@gmail.com>
2025-12-12 10:25:37 +0200

    icons

--- cmd/demo/icons/ui.go
+++ cmd/demo/icons/ui.go
@@ -6,32 +6,29 @@ import (
 	"go-compose-dev/compose/foundation/layout/column"
 	"go-compose-dev/compose/foundation/layout/row"
 
-	"golang.org/x/exp/shiny/materialdesign/icons"
-
 	"go-compose-dev/pkg/api"
 )
 
 func UI(c api.Composer) api.LayoutNode {
+	var rows []api.Composable
+	chunkSize := 40
+
+	for i := 0; i < len(List); i += chunkSize {
+		end := i + chunkSize
+		if end > len(List) {
+			end = len(List)
+		}
+
+		var rowItems []api.Composable
+		for _, def := range List[i:end] {
+			rowItems = append(rowItems, icon.Icon(def.Data))
+		}
+		rows = append(rows, row.Row(compose.Sequence(rowItems...)))
+	}
 
 	c = column.Column(
-		compose.Sequence(
-			row.Row(
-				compose.Sequence(
-					icon.Icon(icons.ContentMail),
-					icon.Icon(icons.SocialNotifications),
-					icon.Icon(icons.ActionAccountCircle),
-				),
-			),
-			row.Row(
-				compose.Sequence(
-					icon.Icon(icons.ContentMail),
-					icon.Icon(icons.SocialNotifications),
-					icon.Icon(icons.ActionAccountCircle),
-				),
-			),
-		),
+		compose.Sequence(rows...),
 	)(c)
 
 	return c.Build()
-
 }
