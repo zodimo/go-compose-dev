131abfd27c58e4adc70b82fa9c9949af133d8e65
Jaco Labuschagne <zodimo@gmail.com>
2025-12-16 08:18:34 +0200

    add more convenience helpers

--- store/typed_helpers.go
+++ store/typed_helpers.go
@@ -18,6 +18,22 @@ func Remember[T any](c state.SupportState, key string, calc func() T) (T, error)
 	return tValue, nil
 }
 
+func RememberUnsafe[T any](c state.SupportState, key string, calc func() T) T {
+	anyCalc := func() any { return calc() }
+	anyValue := c.Remember(key, anyCalc)
+
+	tValue, ok := anyValue.(T)
+	if !ok {
+		var zero T
+		panic(fmt.Errorf("value is not of type %T", zero))
+	}
+	return tValue
+}
+
+func MustRemember[T any](c state.SupportState, key string, calc func() T) T {
+	return RememberUnsafe[T](c, key, calc)
+}
+
 func State[T any](c state.SupportState, key string, initial func() T) (TypedMutableValueInterface[T], error) {
 	mv := c.State(key, func() any { return initial() })
 	anyMv, ok := mv.(*MutableValue)
@@ -25,4 +41,21 @@ func State[T any](c state.SupportState, key string, initial func() T) (TypedMuta
 		return nil, fmt.Errorf("mutable value is not of type %T", MutableValue{})
 	}
 	return WrapMutableValue[T](anyMv)
+}
+
+func StateUnsafe[T any](c state.SupportState, key string, initial func() T) TypedMutableValueInterface[T] {
+	mv := c.State(key, func() any { return initial() })
+	anyMv, ok := mv.(*MutableValue)
+	if !ok {
+		panic(fmt.Errorf("mutable value is not of type %T", MutableValue{}))
+	}
+	wrapped, err := WrapMutableValue[T](anyMv)
+	if err != nil {
+		panic(err)
+	}
+	return wrapped
+}
+
+func MustState[T any](c state.SupportState, key string, initial func() T) TypedMutableValueInterface[T] {
+	return StateUnsafe[T](c, key, initial)
 }
