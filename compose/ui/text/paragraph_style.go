4a5fe75140d4edeb9939fabc60e50a24c65e9075
Jaco Labuschagne <zodimo@gmail.com>
2025-12-21 11:41:01 +0200

    with annotatedString

---
+++ compose/ui/text/paragraph_style.go
@@ -0,0 +1,24 @@
+package text
+
+// https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/ui/ui-text/src/commonMain/kotlin/androidx/compose/ui/text/ParagraphStyle.kt;drc=4970f6e96cdb06089723da0ab8ec93ae3f067c7a;bpv=1;bpt=1;l=66
+type ParagraphStyle struct{}
+
+// textAlign = textAlign ?: TextAlign.Unspecified,
+// textDirection = textDirection ?: TextDirection.Unspecified,
+// lineHeight = lineHeight,
+// textIndent = textIndent,
+// platformStyle = null,
+// lineHeightStyle = null,
+// lineBreak = LineBreak.Unspecified,
+// hyphens = Hyphens.Unspecified,
+// textMotion = null,
+
+// val textAlign: TextAlign = TextAlign.Unspecified,
+// val textDirection: TextDirection = TextDirection.Unspecified,
+// val lineHeight: TextUnit = TextUnit.Unspecified,
+// val textIndent: TextIndent? = null,
+// val platformStyle: PlatformParagraphStyle? = null,
+// val lineHeightStyle: LineHeightStyle? = null,
+// val lineBreak: LineBreak = LineBreak.Unspecified,
+// val hyphens: Hyphens = Hyphens.Unspecified,
+// val textMotion: TextMotion? = null,
---
+++ compose/ui/text/span_style.go
@@ -0,0 +1,21 @@
+package text
+
+// https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/ui/ui-text/src/commonMain/kotlin/androidx/compose/ui/text/SpanStyle.kt;l=279;drc=4970f6e96cdb06089723da0ab8ec93ae3f067c7a
+type SpanStyle struct{}
+
+// color: Color = Color.Unspecified,
+// fontSize: TextUnit = TextUnit.Unspecified,
+// fontWeight: FontWeight? = null,
+// fontStyle: FontStyle? = null,
+// fontSynthesis: FontSynthesis? = null,
+// fontFamily: FontFamily? = null,
+// fontFeatureSettings: String? = null,
+// letterSpacing: TextUnit = TextUnit.Unspecified,
+// baselineShift: BaselineShift? = null,
+// textGeometricTransform: TextGeometricTransform? = null,
+// localeList: LocaleList? = null,
+// background: Color = Color.Unspecified,
+// textDecoration: TextDecoration? = null,
+// shadow: Shadow? = null,
+// platformStyle: PlatformSpanStyle? = null,
+// drawStyle: DrawStyle? = null,
