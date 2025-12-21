1e98dedfe7a2040a04287b29ab8a12303ab59bda
Jaco Labuschagne <zodimo@gmail.com>
2025-12-21 16:12:53 +0200

    add types to conts

--- pkg/floatutils/compare.go
+++ pkg/floatutils/compare.go
@@ -3,8 +3,8 @@ package floatutils
 import "math"
 
 const (
-	Float64EqualityThreshold = 1e-9
-	Float32EqualityThreshold = 1e-6
+	Float64EqualityThreshold float64 = 1e-9
+	Float32EqualityThreshold float32 = 1e-6
 )
 
 // floatEquals compares two float32 values with absolute epsilon tolerance.
