136a0621afc6442a4d59ebbc23be374f1835e56b
Jaco Labuschagne <zodimo@gmail.com>
2025-12-22 11:20:19 +0200

    improve theme color_helper

--- theme/color_helpers.go
+++ theme/color_helpers.go
@@ -3,5 +3,9 @@ package theme
 import "image/color"
 
 func toNRGBA(c color.Color) color.NRGBA {
-	return c.(color.NRGBA)
+	if nrgba, ok := c.(color.NRGBA); ok {
+		return nrgba
+	}
+	nrgbaModel := color.NRGBAModel
+	return nrgbaModel.Convert(c).(color.NRGBA)
 }
