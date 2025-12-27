2581203f08f6914495ca4cc9ec971c0507065158
Jaco Labuschagne <zodimo@gmail.com>
2025-12-27 13:41:27 +0200

    LocalLayoutDirection

---
+++ compose/ui/platform/local_layout_direction.go
@@ -0,0 +1,12 @@
+package platform
+
+import (
+	"github.com/zodimo/go-compose/compose"
+	"github.com/zodimo/go-compose/compose/ui/unit"
+)
+
+// LocalLayoutDirection is a CompositionLocal that provides the layout direction to the composition.
+// This allows components to adapt their layout for left-to-right (LTR) or right-to-left (RTL) languages.
+var LocalLayoutDirection = compose.StaticCompositionLocalOf[unit.LayoutDirection](func() unit.LayoutDirection {
+	panic("CompositionLocal LocalLayoutDirection not present")
+})
