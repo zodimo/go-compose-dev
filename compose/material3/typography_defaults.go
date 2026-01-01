1039e61ae9b5936298256843a4ee1f2f82e97fb9
Jaco Labuschagne <zodimo@gmail.com>
2026-01-01 16:44:11 +0200

    TypographyUnspecified

---
+++ compose/material3/typography_defaults.go
@@ -0,0 +1,45 @@
+package material3
+
+import "github.com/zodimo/go-compose/compose/ui/text"
+
+var TypographyUnspecified = &Typography{
+	DisplaySmall:  text.TextStyleUnspecified,
+	DisplayMedium: text.TextStyleUnspecified,
+	DisplayLarge:  text.TextStyleUnspecified,
+
+	HeadlineSmall:  text.TextStyleUnspecified,
+	HeadlineMedium: text.TextStyleUnspecified,
+	HeadlineLarge:  text.TextStyleUnspecified,
+
+	TitleSmall:  text.TextStyleUnspecified,
+	TitleMedium: text.TextStyleUnspecified,
+	TitleLarge:  text.TextStyleUnspecified,
+
+	BodySmall:  text.TextStyleUnspecified,
+	BodyMedium: text.TextStyleUnspecified,
+	BodyLarge:  text.TextStyleUnspecified,
+
+	LabelSmall:  text.TextStyleUnspecified,
+	LabelMedium: text.TextStyleUnspecified,
+	LabelLarge:  text.TextStyleUnspecified,
+
+	DisplaySmallEmphasized:  text.TextStyleUnspecified,
+	DisplayMediumEmphasized: text.TextStyleUnspecified,
+	DisplayLargeEmphasized:  text.TextStyleUnspecified,
+
+	HeadlineSmallEmphasized:  text.TextStyleUnspecified,
+	HeadlineMediumEmphasized: text.TextStyleUnspecified,
+	HeadlineLargeEmphasized:  text.TextStyleUnspecified,
+
+	TitleSmallEmphasized:  text.TextStyleUnspecified,
+	TitleMediumEmphasized: text.TextStyleUnspecified,
+	TitleLargeEmphasized:  text.TextStyleUnspecified,
+
+	BodySmallEmphasized:  text.TextStyleUnspecified,
+	BodyMediumEmphasized: text.TextStyleUnspecified,
+	BodyLargeEmphasized:  text.TextStyleUnspecified,
+
+	LabelSmallEmphasized:  text.TextStyleUnspecified,
+	LabelMediumEmphasized: text.TextStyleUnspecified,
+	LabelLargeEmphasized:  text.TextStyleUnspecified,
+}
